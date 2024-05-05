290784910a3a4441edb64dbc341ac1fe41d3cf8b
Declan O’Brien <80363579+declanobrien03@users.noreply.github.com>
2024-05-04 20:55:37 -0400

    remove api call

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
-  useEffect(() => {
-    const go = async () => {
-      const response = await fetch('https://3.148.144.236/api/test')
-      const data = await response.json()
-      setMessage(data)
-    }
-    go().catch(alert)
-  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
-        <p>{message}</p>
       </header>
     </div>
   );
